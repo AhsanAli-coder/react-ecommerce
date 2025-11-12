b865280138da286f4be68a75ab87b948986d5e97
Ahsan Ali <ahsanriaz0475@gmail.com>
2025-11-13 01:25:49 +0500

    feteh data from the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route
-          path="checkout"
-          element={<div>hallo checkout page </div>}
-        ></Route>
+        <Route path="checkout" element={<CheckoutPage/>}></Route>
+        <Route path="orders" element={<OrdersPage/>}></Route>
       </Routes>
     </>
   );
